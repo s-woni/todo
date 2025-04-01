88986136cb8e6657152e17d4c6df9bb2c074fd65
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-04-01 23:59:35 +0900

    Env: Member & Schedule Entity 생성

---
+++ src/main/java/com/example/todo/entity/Member.java
@@ -0,0 +1,21 @@
+package com.example.todo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "member")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
---
+++ src/main/java/com/example/todo/entity/Schedule.java
@@ -0,0 +1,26 @@
+package com.example.todo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "schedule")
+public class Schedule extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String contents;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+
+}
